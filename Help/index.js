3266bcd85caeeea0d30e4bd2a3cfeaed2d0085ea
Kurdaszow <tomek@kudraszow.pl>
2026-05-19 01:18:54 +0200

    Add files via upload

--- Help/index.js
+++ Help/index.js
@@ -1,33 +1,65 @@
 
-const games =
-[
-{title: "Noita",playtime_recent: 13,playtime_all:56,img_src:"noita.png"},
-{title: "The Binding Of Isaac: Rebirth",playtime_recent: 3,playtime_all:190,img_src:"isaac.png"},
-{title: "Mewgenics",playtime_recent: 24,playtime_all:27,img_src:"mew.png"},
+let games2=[];
+let playerdata=[];
+async function getGameData() {
+    const response = await fetch("./a.json");//https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=8CBCBCA99725FD3101494787D8319310&steamid=76561198821125519&format=json&include_appinfo=1
 
-];
+    return await response.json();
+}
+async function getUserData() {
+    const response = await fetch("./b.json");//https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=8CBCBCA99725FD3101494787D8319310&steamids=76561198821125519&format=json
+    return await response.json();
+}
+
 let filterid=2;
 let ascending=true
-const filters=["title","playtime_recent","playtime_all"]
+const filters=["name","playtime_2weeks","playtime_forever"]
 let gamediv;
-document.addEventListener("DOMContentLoaded",()=> {
-gamediv = document.getElementById("games");   
-Filter(filters,ascending)
+document.addEventListener("DOMContentLoaded", async ()=> {
+games2 = await getGameData();
+playerdata = await getUserData();
+ 
+
+GenerateHtml()
+gamediv = document.getElementById("games");
+Filter(filters,ascending)  
 DisplayGames()   
 })
+
+function GenerateHtml(){
+    let section = document.createElement("section")
+    section.id = "main"
+    let divv = document.createElement("div")
+    divv.className = "ioknow"
+    let imagi = document.createElement("img")
+    imagi.src = playerdata.response.players[0].avatarfull
+    imagi.className = "profilepic"
+    let h3 = document.createElement("h3")
+    h3.textContent = playerdata.response.players[0].personaname
+
+    divv.appendChild(imagi)
+    divv.appendChild(h3)
+    section.appendChild(divv)
+    let gamesDiv = document.createElement("div");
+    gamesDiv.id = "games";
+    section.appendChild(gamesDiv);
+    document.body.appendChild(section)
+    
+
+}
 
 function Filter(list,ascend){
 
 
-    for(let i = 0;i<games.length-1;i++){
-        for(let j = 0;j<games.length-1;j++){
+    for(let i = 0;i<games2.response.games.length-1;i++){
+        for(let j = 0;j<games2.response.games.length-1;j++){
             if(ascend===true){
-                if(games[j][list[filterid]]>games[j+1][list[filterid]]){
-                [games[j],games[j+1]] = [games[j+1],games[j]]
+                if((games2.response.games[j][list[filterid]] || 0) >(games2.response.games[j+1][list[filterid]] || 0)){
+                [games2.response.games[j],games2.response.games[j+1]] = [games2.response.games[j+1],games2.response.games[j]]
             } 
             }else if(ascend===false){
-                if(games[j][list[filterid]]<games[j+1][list[filterid]]){
-                [games[j],games[j+1]] = [games[j+1],games[j]]
+                if((games2.response.games[j][list[filterid]] || 0) <(games2.response.games[j+1][list[filterid]] || 0)){
+                [games2.response.games[j],games2.response.games[j+1]] = [games2.response.games[j+1],games2.response.games[j]]
             }                 
             }
         }
@@ -49,7 +81,7 @@ nrofgames.className = "nrofgames"
 let btn1 = document.createElement("button")
 btn1.textContent=`filter by: ${filters[filterid]}`
 let nrp = document.createElement("p")
-nrp.textContent = `this user owns ${games.length} games`
+nrp.textContent = `this user owns ${games2.response.game_count} games`
 let btn2 = document.createElement("button")
 if(ascending){
     btn2.textContent="Ascending"
@@ -74,23 +106,27 @@ nrofgames.append(divbtn)
 nrofgames.appendChild(nrp)
 
 gamediv.appendChild(nrofgames)
-games.forEach(game => {
+let gamescont = document.createElement("div");
+gamescont.className="gamescont"
+games2.response.games.forEach(game => {
     let gradiv = document.createElement("div");
     gradiv.className="gra";
     let graheader = document.createElement("div");
     graheader.className="gra-header"
     let image = document.createElement("img");
-    image.src =game.img_src;
+    let imgsrc = `https://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg`;
+
+    image.src =imgsrc;
     image.className="gamemin"
     let text5 = document.createElement("h5")
-    text5.textContent = game.title
+    text5.textContent = game.name
     let link = document.createElement("a")
     link.className="nwm"
     link.href="#"
     link.textContent="See Details"
     let playtime = document.createElement("p")
     playtime.className="playtime"
-    playtime.textContent =`Last to weeks: ${game.playtime_recent}h \nAll time: ${game.playtime_all}h`
+    playtime.textContent =`Last to weeks: ${((game.playtime_2weeks || 0)/60).toFixed(2)}h \nAll time: ${(game.playtime_forever/60).toFixed(2)}h`
     playtime.setAttribute('style','white-space: pre')
     text5.appendChild(link)
     
@@ -98,6 +134,7 @@ games.forEach(game => {
     graheader.appendChild(text5)
     gradiv.appendChild(graheader)
     gradiv.appendChild(playtime)
-    gamediv.appendChild(gradiv);
+    gamescont.appendChild(gradiv);
 });
+gamediv.appendChild(gamescont);
 }
